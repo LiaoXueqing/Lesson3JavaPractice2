c0f663a65c46ea47df7877c7ea1683a7c5e753cf
xuexue <1047504737@qq.com>
2018-04-13 17:51:16 +0800

    Lesson3 Question2 practice10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,19 @@
 package practice10;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+    public Student(int id,String name,int age,Klass klass){
+        super(id,name,age);
+        this.klass = klass;
+    }
+    public String introduce(){
+        return super.introduce()+" I am a Student. I am "+(this == klass.getLeader()?"Leader of "+klass.getDisplayName()+".":"at "+klass.getDisplayName()+".");
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,42 @@
 package practice10;
 
-public class Teacher {
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person{
+    private HashSet<Klass> classes = new HashSet<>();
+    private final String JOB_INTRODUCE = " I am a Teacher.";
+    public Teacher(int id,String name,int age,HashSet<Klass> classes){
+        super(id,name,age);
+        this.classes = classes;
+    }
+    public Teacher(int id,String name,int age){
+        super(id,name,age);
+    }
+    private String myClassesName() {
+        StringBuilder sb = new StringBuilder("Class ");
+        this.classes.forEach(klass -> sb.append(klass.getNumber() + ", "));
+        return sb.substring(0, sb.length() - 2);
+    }
+    public String introduce(){
+        return super.introduce()+JOB_INTRODUCE+" I teach "+((this.classes==null || getClasses().size()==0)?"No Class.":""+ this.myClassesName() +".");
+    }
+
+    public String introduceWith(Student st) {
+        if(isTeaching(st)){
+            return super.introduce()+JOB_INTRODUCE+" I teach "+st.getName()+".";
+        }
+        return super.introduce()+JOB_INTRODUCE+" I don't teach "+st.getName()+".";
+    }
+    public boolean isTeaching(Student student){
+        return this.classes.stream().filter(klass -> klass.isIn(student)).collect(Collectors.toSet()).size() > 0;
+    }
+
+    public HashSet<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(HashSet<Klass> classes) {
+        this.classes = classes;
+    }
 }
