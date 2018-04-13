e54d1f929df3688e1faba0368a6f4ce7e7909988
xuexue <1047504737@qq.com>
2018-04-13 15:05:31 +0800

    Lesson3 Question2 practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,22 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass=0;
+    public Teacher(String name,int age,int klass){
+        super(name,age);
+        this.klass = klass;
+    }
+    public Teacher(String name,int age){
+        super(name,age);
+    }
+    public String introduce(){
+        return super.introduce()+" I am a Teacher. I teach "+(klass==0?"No Class.":"Class "+getKlass()+".");
+    }
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }
