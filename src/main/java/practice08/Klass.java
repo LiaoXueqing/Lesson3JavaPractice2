60e668c9e76dddba8c53b66f1303c1092cfbcc1b
xuexue <1047504737@qq.com>
2018-04-13 15:15:41 +0800

    Lesson3 Question2 practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,29 @@
 package practice08;
 
 public class Klass {
+    private int number = 0;
+    public Student leader;
+    public Klass(int number){
+        this.number = number ;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        return "Class "+getNumber();
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,28 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+    private final String JOB_INTRODUCE = " I am a Teacher.";
+    public Teacher(int id,String name,int age,Klass klass){
+        super(id,name,age);
+        this.klass = klass;
+    }
+    public Teacher(int id,String name,int age){
+        super(id,name,age);
+    }
+    public String introduce(){
+        return super.introduce()+JOB_INTRODUCE+" I teach "+(klass==null?"No Class.":""+klass.getDisplayName()+".");
+    }
+
+    public String introduceWith(Student st) {
+        return super.introduce()+JOB_INTRODUCE+(st.getKlass().getNumber()==getKlass().getNumber()?" I teach "+st.getName()+".":" I don't teach "+st.getName()+".");
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
