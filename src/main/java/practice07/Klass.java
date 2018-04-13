bf99c79855bb72384199416dddc3b8aa7a3385f0
xuexue <1047504737@qq.com>
2018-04-13 15:10:16 +0800

    Lesson3 Question2 practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,20 @@
 package practice07;
 
 public class Klass {
+    int number = 0;
+
+    public Klass(int number){
+        this.number = number ;
+    }
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        return "Class "+getNumber();
+    }
 }
